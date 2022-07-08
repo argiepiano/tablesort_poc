151f8e30dee735cd836a8c1beea828c381199a2c
argiepiano <argiepiano@yahoo.com>
2022-07-07 18:55:08 -0600

    Added a bit of documentation to the js file

--- js/tablesort_trigger.js
+++ js/tablesort_trigger.js
@@ -1,6 +1,15 @@
 (function ($) {
   Backdrop.behaviors.attachMyTablesort = {
     attach: function (context, settings) {
+      // This is the most basic setting (see the tablesort github for more options).
+
+      // This is hard-coded to look for a table with ID: 'table-tablesort'.
+
+      // Change this ID if you so desire, or better yet, pass the ID to the browser by using 
+      // the 'setting' key in the $options parameter of backdrop_add_js. 
+      // To learn how to pass settings see:
+      // https://www.drupal.org/docs/7/api/javascript-api/managing-javascript#settings-javascript
+      // 
       new Tablesort(document.getElementById('table-tablesort'));
     }
   }
