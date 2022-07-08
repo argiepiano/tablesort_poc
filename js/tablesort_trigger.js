4e042b13e77b2fe02c550112c4b33ab51b2efe2e
argiepiano <argiepiano@yahoo.com>
2022-07-07 18:46:52 -0600

    Removed console.log

--- js/tablesort_trigger.js
+++ js/tablesort_trigger.js
@@ -1,8 +1,6 @@
 (function ($) {
   Backdrop.behaviors.attachMyTablesort = {
     attach: function (context, settings) {
-      console.log('triggered');
-      console.log(document.getElementById('table-tablesort'));
       new Tablesort(document.getElementById('table-tablesort'));
     }
   }
